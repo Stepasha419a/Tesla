446465fa86e3e25ffd53f32a1f274e66902fd1eb
Stepasha419a <Dark_Magic419a@mail.ru>
2022-07-03 21:05:46 +0300

    working menu

--- js/main.js
+++ js/main.js
@@ -6,4 +6,11 @@ $(function() {
         autoplay: true,
         autoplaySpeed: 3000
     })
+
+    $('.header__burger-btn').on('click', () => {
+        $('.menu').addClass('menu--active')
+    })
+    $('.menu__close-btn').on('click', () => {
+        $('.menu').removeClass('menu--active')
+    })
 })
